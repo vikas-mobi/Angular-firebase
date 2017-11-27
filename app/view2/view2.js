9fde7a9b77a16bf14e59ef135f73e71c4e649b3f
vikas kushwaha <vikas.kushwaha@mobiloittegroup.com>
2017-11-27 13:56:08 +0530

    change Auth type

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,7 +2,7 @@
 mainApp.controller('View2Ctrl', ['$scope', '$window', function($scope, $window) {
 const auth = firebase.auth()
 $scope.signUp = function(){
- var promise = auth.createUserWithEmailAndPassword($scope.email, $scope.pass)
+ var promise = auth.signInWithEmailAndPassword($scope.email, $scope.pass)
 
  promise.catch(e =>{
    console.log(e.message);
